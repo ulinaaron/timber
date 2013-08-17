9266e9c38c780ad2486746848bb11900d35eb06f
Jared Novack <jarednova@upstatement.com>
2013-08-17 09:40:55 -0400

    more updates to rh

--- slides/wordcamp-pvd-randomhouse/js/timber-slides.js
+++ slides/wordcamp-pvd-randomhouse/js/timber-slides.js
@@ -7,14 +7,17 @@
 	function toNumber(val){var str=String(val);str=str.replace(/[^\d.-]/g,"");var num=Number(str);return num;}
 
 	TimberSlides.prototype.initCounters = function(){
-		$('.counter').each(function(){
-			var $counter = $(this);
-			var from = 0;
-			if ($counter.data('from')){
-				from = $counter.data('from');
-			}
-			$counter.countTo({to:toNumber($counter.text()), from:from, speed:8000});
-		});
+		Reveal.addEventListener( 'has-counter', function() {
+			$('.counter').each(function(){
+				var $counter = $(this);
+				var from = 0;
+				if ($counter.data('from')){
+					from = $counter.data('from');
+				}
+				$counter.countTo({to:toNumber($counter.text()), from:from, speed:2400});
+			});
+		} );
+		
 	};
 
 	new TimberSlides();
